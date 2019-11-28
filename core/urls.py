0aacbf7fd54f5b85f2b638aedd4b53f1be8f88ed
danvildev666 <danvillano666@gmail.com>
2019-11-28 11:29:09 -0300

    Limpiesa de codigo

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib import admin
-from .views import home,login,logout,register,Registro_usuario,eventos,galeria,info,contacto,carta,reserva
+from .views import home,login,logout,Registro_usuario,eventos,galeria,info,contacto,carta,reserva
 from core import views
 urlpatterns = [
     path('',home, name='home'),
@@ -10,7 +10,7 @@ urlpatterns = [
     path('galeria/',galeria, name='galeria'),
     path('info/',info, name='info'),
     path('contacto/',contacto, name='contacto'),
-    path('register/', register, name='register'),
+    
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
     path('logout/', logout, name='logout'),
