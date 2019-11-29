4facfce8a73fffe86f551d44dcd7403478f0f2cc
danvildev666 <danvillano666@gmail.com>
2019-11-29 08:32:14 -0300

    add request login into models

--- core/views.py
+++ core/views.py
@@ -12,7 +12,7 @@ from .models import Reserva
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
-
+@login_required
 def eventos(request):
     return render(request, "core/eventos.html")
 
